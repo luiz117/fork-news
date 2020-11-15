c1ce9227dcf813b36c4cc65f23659727d1d2b4f7
Vitor Nunes <vitornovictor@gmail.com>
2020-11-15 18:23:16 +0000

    Add a PR reviewer

--- pr_step.sh
+++ pr_step.sh
@@ -9,10 +9,19 @@ readonly FROM=$3
 readonly TO=$4
 readonly TITLE=$5
 readonly BODY=$6
+readonly REVIEWERS=$7
 
-curl -s \
+readonly fork_news_pr=$(curl -s \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $ACCESS_TOKEN" \
   https://api.github.com/repos/"$REPO"/pulls \
-  -d "{\"head\": \"$FROM\",\"base\":\"$TO\", \"title\": \"$TITLE\", \"body\": \"$BODY\"}"
+  -d "{\"head\": \"$FROM\",\"base\":\"$TO\", \"title\": \"$TITLE\", \"body\": \"$BODY\"}")
+
+readonly pr_id=$(echo "$fork_news_pr" | jq -r .number)
+curl -s \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $ACCESS_TOKEN" \
+  https://api.github.com/repos/"$REPO"/pulls/"$pr_id"/requested_reviewers \
+  -d "{\"reviewers\":[\"$REVIEWERS\"]}"
