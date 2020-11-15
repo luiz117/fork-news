30b3c1985fb192f299ca3c90c22c9c7a263a29d6
Vitor Nunes <vitornovictor@gmail.com>
2020-11-15 19:21:25 +0000

    Add optional reviewers

--- pr_step.sh
+++ pr_step.sh
@@ -9,7 +9,7 @@ readonly FROM=$3
 readonly TO=$4
 readonly TITLE=$5
 readonly BODY=$6
-readonly USERS=$7
+readonly REVIEWERS=$7
 
 readonly fork_news_pr=$(curl -s \
   -X POST \
@@ -18,10 +18,14 @@ readonly fork_news_pr=$(curl -s \
   https://api.github.com/repos/"$REPO"/pulls \
   -d "{\"head\": \"$FROM\",\"base\":\"$TO\", \"title\": \"$TITLE\", \"body\": \"$BODY\"}")
 
+if [ -z "$REVIEWERS" ]; then
+      exit 0
+fi
+
 readonly pr_id=$(echo "$fork_news_pr" | jq -r .number)
-curl \
+curl -s \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $ACCESS_TOKEN" \
-  https://api.github.com/repos/"$REPO"/issues/"$pr_id"/comments \
-  -d "{\"body\":\"@$USERS\"}"
+  https://api.github.com/repos/"$REPO"/pulls/"$pr_id"/requested_reviewers \
+  -d "{\"reviewers\":[\"$REVIEWERS\"]}"
